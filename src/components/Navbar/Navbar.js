783b2f1e61e25596e2750c0b35ca9673bd47b599
ahm-rubayed <rubayedahmed04@gmail.com>
2022-09-28 07:45:40 +0600

    Navbar added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ function BasicExample() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Agro</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
@@ -18,10 +18,10 @@ function BasicExample() {
             <Nav.Link href="#link">Shop</Nav.Link>
             <Nav.Link href="#link">About</Nav.Link>
             <Nav.Link href="#link">Contact</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown title="Blog" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Blog1</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Blog2
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
